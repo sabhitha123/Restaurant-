2f9d31901a1a5a36a64890147d79b4aa20256466
sabhitha123 <40881260+sabhitha123@users.noreply.github.com>
2021-10-30 23:02:58 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <img className="restaurant-opening-95"
+                src="restaurant-opening-95.png"
+                alt="restaurant-opening-95"
+                />
+            </div>
+        )
+    }
+}
